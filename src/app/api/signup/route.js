39579f9eaa734dc93b667655d6b013cf58f3b735
Noah Kim <noahskim7@gmail.com>
2024-10-19 02:12:54 -0700

    feat: post for signup

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,22 +1,41 @@
 import { NextResponse } from "next/server";
 import { pool } from "@/config/database";
+import swot from "swot-node";
+import { hash } from "bcrypt";
 
-export async function POST(req, res) {
+export async function POST(req) {
   try {
-    const { first_name, last_name, email } = req.body;
+    const { first_name, last_name, email, password } = req.json();
+
+    // verify email
+    if (!first_name || !last_name || !email || !password) {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
+    }
+
+    if (!swot.isAcademic(email)) {
+      return NextResponse.json(
+        { error: "Please enter a valid school email" },
+        { status: 400 },
+      );
+    }
+
+    // Hash the password
+    const saltRounds = 10;
+    const hashedPassword = await hash(password, saltRounds);
+
     const results = await pool.query(
       `
 	  INSERT INTO users (first_name, last_name, email)
 	  VALUES($1, $2, $3)
 	  RETURNING *`,
-      [first_name, last_name, email],
+      [first_name, last_name, email, hashedPassword],
     );
 
-    res.status(201).json(results.rows[0]);
+    NextResponse.json(results.rows[0], { status: 201 });
   } catch (error) {
-    res.status(409).json({ error: error.message });
+    NextResponse.json({ error: "Some shi went wrong bruh" }, { status: 500 });
   }
 }
-
-// beore query, check if email has .edu at the end
-// if not "please enter your .edu school email"
