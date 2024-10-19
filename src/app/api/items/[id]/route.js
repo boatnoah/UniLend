d0bb050a7e05c6c012fc13ee041848ecd6ef9fb8
Noah Kim <noahskim7@gmail.com>
2024-10-18 23:34:18 -0700

    feat: setup pg and railway connection

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -4,8 +4,7 @@ import { pool } from "@/config/database";
 
 export async function GET() {
   try {
-    const result = await pool.query("SELECT * FROM items");
-    return NextResponse.json({ success: true, data: result.rows });
+    return NextResponse.json({ hello: "world" });
   } catch (error) {
     return NextResponse.json(
       { success: false, error: "Database connection failed" },
