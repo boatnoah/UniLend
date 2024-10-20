adbda54f5c8a2a9b252fbf58b95da5b16bb9797e
cole <colehartman00@gmail.com>
2024-10-20 01:19:10 -0700

    dynamic navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,25 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
 export default function Navbar() {
-  return (
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsSignedIn(true);
+    }
+  }, []);
+
+  const handleSignOut = () => {
+    setIsSignedIn(false);
+    localStorage.removeItem("token");
+  };
+
+  return isSignedIn ? (
     <nav className="bg-transparent p-4">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
@@ -11,7 +28,25 @@ export default function Navbar() {
           </div>
           <div className="flex items-center space-x-4">
             <div className="relative w-8 h-8">Image</div>
-            <Button variant="outline">Log In</Button>
+            <Link href={"/"} onClick={handleSignOut}>
+              <Button variant="outline">Sign Out</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  ) : (
+    <nav className="bg-transparent p-4">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <h1 className="text-2xl font-bold text-primary">UniLend</h1>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="relative w-8 h-8">Image</div>
+            <Link href={"/signin"}>
+              <Button variant="outline">Sign In</Button>
+            </Link>
           </div>
         </div>
       </div>
